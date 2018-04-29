a2c6e69ed062761430015ab70584aa3f7059c2d9
Jack Schuss <jackschuss@gmail.com>
2018-04-29 17:01:14 -0400

    wakeup config

--- app/controllers/jayne_controller.rb
+++ app/controllers/jayne_controller.rb
@@ -2,4 +2,8 @@
    def uptime
      render json: MemeCommands.new(channel: params["channel"]).akm_blade_uptime, status: 200
    end
+
+   def wakeup
+     render json: {}, status: :no_content
+   end
  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "uptime", to: "jayne#uptime"
+  get "wakeup", to: "jayne#wakeup"
 
   namespace :pug_bot do
   end
