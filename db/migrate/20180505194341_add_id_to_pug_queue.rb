ad867ade8df46ab3f1b02d33d82350c6a9ab1296
Jack Schuss <jackschuss@gmail.com>
2018-05-05 15:48:40 -0400

    fix

--- db/migrate/20180505194341_add_id_to_pug_queue.rb
+++ db/migrate/20180505194341_add_id_to_pug_queue.rb
@@ -1,5 +1,5 @@
 class AddIdToPugQueue < ActiveRecord::Migration[5.1]
   def change
-    add_column :queue_spots, :pug_id, :integer
+    add_column :queue_spots, :member_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504195056) do
+ActiveRecord::Schema.define(version: 20180505194341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20180504195056) do
     t.datetime "updated_at", null: false
     t.boolean "captain"
     t.string "region"
+    t.integer "pug_id"
   end
 
 end
