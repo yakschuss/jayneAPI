2fe45d0e0712493ec51d94a9188e48bb628ee7ef
Jack Schuss <jackschuss@gmail.com>
2018-05-31 16:51:18 -0400

    private messages

--- app/services/t_five_bot/bot.rb
+++ app/services/t_five_bot/bot.rb
@@ -24,22 +24,25 @@ module TFiveBot
     def moderate_t500_channel(event, bot)
       prefix = event.message.to_s[0..1]
 
-      puts prefix
-
       if prefix == "M:"
         unless event.user.role?(352683696161030167)
           event.message.delete
-          event.send_message("Uh <@#{event.user.id}>, you're not a mod.")
+          event.send_private_message("Uh, you're not a mod. Please don't use the mod tag to answer questions in #ask-a-t500.", event, bot)
         end
       elsif prefix == "A:"
         unless event.user.role?(434171221156823040)
           event.message.delete
-          event.send_message("<@#{event.user.id}>, You need to have the T500 role to answer questions in this channel. Please message a moderator with proof in order to obtain the role.")
+          event.send_private_message("You need to have the T500 role to answer questions in #ask-a-t500. Please message a moderator with proof in order to obtain the role.", event, bot)
         end
       elsif prefix != "Q:"
         event.message.delete
-        event.send_message("<@#{event.user.id}>, You must prefix all responses with Q: or A: in order to use this channel.")
+        event.send_private_message("You must prefix all responses with Q: or A: in order to use #ask-a-t500.", event, bot)
       end
+    end
+
+    def send_private_message(message, event, bot)
+      channel = bot.pm_channel(event.user.id)
+      bot.send_message(channel, message)
     end
   end
 end
